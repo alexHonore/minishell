0e4a92e343c11b2f05049b35c1a98659569b61db
alexHonore <nsh.alexhonore@gmail.com>
2022-11-17 16:19:32 -0500

    Include

---
+++ includes/minishell.h
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anshimiy <anshimiy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/17 15:26:12 by anshimiy          #+#    #+#             */
+/*   Updated: 2022/11/17 15:53:24 by anshimiy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+/* ****** INCLUDES ****** */
+
+# include "my_lib/mylib.h"
+# include <ctype.h>
+# include <fcntl.h>
+# include <readline/readline.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+# define DBQUOTE 34
+# define SQUOTE 39
+# define PIPE 124
+# define SPACE 32
+# define DOLLAR 36
+# define DOUBLEPIPE 123
+# define DOUBLEAMPERSAND 125
+
+typedef enum e_shell_status {
+    RUNNING,
+    OVER,
+} t_shell_status;
+
+/* ****** STRUCTS ****** */
+
+typedef struct s_env
+{
+	char			*cmd;
+	char			*expand;
+	struct s_env	*next;
+
+}					t_env;
+
+typedef struct s_shell
+{
+	char			*prompt;
+	char			**argv;
+	t_shell_status	shell_status;
+	int				size;
+	int				nb_cmd;
+	char			**envp;
+	t_env			*env;
+
+}					t_shell;
+
+/* ****************************** FUNCTIONS ****************************** */
+
+/* ******   MINISHELL   ****** */
+
+/* ******    PARSING    ****** */
+
+/* ******     INIT      ****** */
+
+/* ******     UTILS     ****** */
+
+/* ******      ENV      ****** */
+
+/* ******     LOGS      ****** */
+
+/* ******   EXECUTION   ****** */
+
+
+#endif
